20ee1fbc8f057d7a674c178bd0b6fb9f6c7c0f0b
vaibhavabg <vaibhavabg1234@gmail.com>
2024-05-20 22:05:28 +0530

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import LoginPage from './Pages/LoginPage';
 
@@ -7,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <LoginPage/>
+    
     </div>
   );
 }
